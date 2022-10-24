011ee0fa0ee1c514e0007ca0467374a619cc1e2b
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-25 00:40:46 +0600

    Navigationbar Component Created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import Navigationbar from './components/Navigationbar/Navigationbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Hello !! I'm gonna start a new project</h1>
-       <Button className='btn btn-warning'>Get Started</Button>
-      </header>
+       <Navigationbar></Navigationbar>
     </div>
   );
 }
