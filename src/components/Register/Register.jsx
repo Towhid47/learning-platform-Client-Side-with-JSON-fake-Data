b0bc936dc58b758416fac436046640716b528d84
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-26 09:36:26 +0600

    Email Validation Complete

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
@@ -6,6 +6,35 @@ import { Link } from 'react-router-dom';
 
 
 const Register = () => {
+
+    const [name , setName] = useState("");
+     const [email , setEmail] = useState("");
+    // const [password , setPassword] = useState("");
+     const [error , setError] = useState("");
+
+   const handleName= (e) =>{
+      const name = (e.target.value);
+        setName(name);
+    }
+
+    const handleEmail = (e) =>{
+        const email = e.target.value;
+
+        /////// Email Validation //////////
+        var isValidEmail =  /\S+@\S+\.\S+/.test(email);
+
+        if(!isValidEmail){
+            setError("invalid Email. Please Enter Your valid Email.");
+            return;
+        }
+        else{
+            setEmail(email);
+            setError("");
+        }
+    }
+
+ 
+
     return (
         <div>
                <div className='container d-flex flex-column justify-content-center w-100 align-items-center'>
@@ -14,27 +43,28 @@ const Register = () => {
                     <p className='fw-bold text-center'>Create Your New Account to get our Latest updates</p>
                 </div>
 
-                <div className='ms-5 w-50 mt-4 p-5 border border-3 border-dark rounded-4'>
+                <div className='ms-5 w-50 mt-4 p-5  border border-3 border-dark rounded-4'>
+                    <p className='text-center text-danger'>{error}</p>
                                 {/* ///////// LogIn Form ///////////// */}
-                        <Form className='w-100'>
+                        <Form className='w-100 mt-4'>
                                 <Form.Group className="mb-3" controlId="">
                                     <Form.Label className='fw-semibold fs-3'>Enter Your Name</Form.Label>
-                                    <Form.Control type="text" placeholder="Full Name" className='border border-dark border-2' />
+                                    <Form.Control type="text" onBlur={handleName} placeholder="Full Name" className='border border-dark border-2' required/>
                                     <Form.Text className="text-muted">
                                     </Form.Text>
                                 </Form.Group>
                                
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
                                     <Form.Label className='fw-semibold fs-3'>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" className='border border-dark border-2' />
-                                    <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
+                                    <Form.Control type="email" onBlur={handleEmail} placeholder="Enter email" className='border border-dark border-2' required/>
+                                    <Form.Text className="text-danger">
+                                     
                                     </Form.Text>
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     <Form.Label className='fw-semibold fs-3'>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" className='border border-dark border-2' />
+                                    <Form.Control type="password" placeholder="Password" className='border border-dark border-2' required/>
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                                     <Form.Check type="checkbox" label="Check me out" />
